db005232b70c9e30ea569bd770b3ab01c5c332a3
NoahTheCodeMaker <noahdragoon@hotmail.com>
2023-03-10 11:26:37 -0500

    added setup.sh to gitignore

--- setup.sh
+++ setup.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 export DATABASE_URL="postgresql://postgres:postgres@localhost:5432/postgres"
 export EXCITED="true"
+export FLASK_APP=app.py
+export FLASK_ENV=development
 echo "setup.sh script executed successfully!"
