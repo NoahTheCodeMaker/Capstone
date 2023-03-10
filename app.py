14423cbcd16a5b5179b38e49b4a68a127703c3a7
NoahTheCodeMaker <noahdragoon@hotmail.com>
2023-03-10 14:09:25 -0500

    Added root, login, and auth endpoints

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 import os
-from flask import Flask, jsonify
+from flask import Flask, jsonify, redirect, request
+from auth import requires_auth
 from models import setup_db
 from flask_cors import CORS
+
+LOGIN_LINK = os.environ['LOGIN_LINK']
 
 def create_app(test_config=None):
 
@@ -9,10 +12,28 @@ def create_app(test_config=None):
     setup_db(app)
     CORS(app)
 
+    # Route that works for anyone with the link
     @app.route('/')
     def capstone_intro():
         return jsonify({
             "message": "Welcome to my Capstone Project!"
+        })
+    
+    # Login route that works for anyone with the link
+    @app.route('/login')
+    def login():
+        return(redirect(location=LOGIN_LINK))
+
+    # You can check the /auth endpoint to see
+    # if the login worked with your own jwt,
+    # but the rest of the endpoints need my 
+    # custom roles on the tokens provided
+    @app.route('/auth')
+    @requires_auth("")
+    def authorize(payload):
+        return jsonify({
+            "success": True,
+            "payload": payload
         })
 
     return app
