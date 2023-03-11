278dab89afc30529f7ec0a41f5ca8f58e8f08365
NoahTheCodeMaker <noahdragoon@hotmail.com>
2023-03-11 11:01:30 -0500

    Added Error handlers

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
-import os
-from flask import Flask, jsonify, redirect, request
-from auth import requires_auth
-from models import setup_db
+import os, traceback
+from flask import Flask, jsonify, redirect, abort, request
+from auth import requires_auth, AuthError
+from models import setup_db, Actors, Movies, db
 from flask_cors import CORS
 
 LOGIN_LINK = os.environ['LOGIN_LINK']
@@ -16,7 +16,7 @@ def create_app(test_config=None):
     @app.route('/')
     def capstone_intro():
         return jsonify({
-            "message": "Welcome to my Capstone Project!"
+            "message": "Welcome to my Capstone Project! Please navigate to /login to log into an account!"
         })
     
     # Login route that works for anyone with the link
@@ -36,6 +36,57 @@ def create_app(test_config=None):
             "payload": payload
         })
 
+    # Error handlers
+
+    @app.errorhandler(400)
+    def bad_request(error):
+        return jsonify({
+        "success": False,
+        "error": 400,
+        "message": "bad request"
+        }), 400 
+
+    @app.errorhandler(404)
+    def not_found(error):
+        return jsonify({
+        "success": False,
+        "error": 404,
+        "message": "resource not found"
+        }), 404
+    
+    @app.errorhandler(405)
+    def not_allowed(error):
+        return jsonify({
+        "success": False,
+        "error": 405,
+        "message": "method not allowed"
+        }), 405
+
+    @app.errorhandler(422)
+    def unprocessable_entity(error):
+        return jsonify({
+        "success": False,
+        "error": 422,
+        "message": "unprocessable entity"
+        }), 422
+
+    @app.errorhandler(500)
+    def internal_server_error(error):
+        return jsonify({
+        "success": False,
+        "error": 500,
+        "message": "internal server error"
+        }), 500
+    
+    # Authorization error handler
+    @app.errorhandler(AuthError)
+    def auth_error(payload):
+        return jsonify({
+            "success": False,
+            "error": payload.status_code,
+            "message": payload.error
+        }), payload.status_code
+    
     return app
 
 app = create_app()
